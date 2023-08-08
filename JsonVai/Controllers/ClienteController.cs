573c12cc791bb50136c19e8bdf15f56e3e799f5c
Edson Romero <Edson.zaken@gmail.com>
2023-08-08 02:35:22 -0300

    ChatGPT

--- JsonVai/Controllers/ClienteController.cs
+++ JsonVai/Controllers/ClienteController.cs
@@ -1,9 +1,11 @@
 ﻿using JsonVai.Models;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using OpenAI_API;
 using OpenAI_API.Completions;
 using System.Linq;
+using System.Text;
 
 namespace JsonVai.Controllers
 {
@@ -99,11 +101,24 @@ namespace JsonVai.Controllers
         {
             return View();
         }
-
+        /*
         [HttpPost]
         public IActionResult Pesquisar(string pergunta)
         {
-            var respostaAssistente = PesquisarComChatGPT(pergunta);
+            var respostaAssistente = PesquisarComCatGPT(pergunta);
+
+            var clientes = _jsonFileService.LerDados();
+            var resultados = clientes.Where(c => c.Nome.Contains(respostaAssistente, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            ViewBag.Pergunta = pergunta;
+            ViewBag.Resultados = resultados;
+
+            return View();
+        }*/
+        [HttpPost]
+        public IActionResult Pesquisar(string pergunta)
+        {
+            var respostaAssistente = PesquisarComCatGPT(pergunta);
 
             var clientes = _jsonFileService.LerDados();
             var resultados = clientes.Where(c => c.Nome.Contains(respostaAssistente, StringComparison.OrdinalIgnoreCase)).ToList();
@@ -114,7 +129,40 @@ namespace JsonVai.Controllers
             return View();
         }
 
-        private string PesquisarComChatGPT(string pergunta)
+        private string PesquisarComCatGPT(string pergunta)
+        {
+            string apiKey = "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";
+            string resposta = string.Empty;
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+                var prompt = $"Usuário: {pergunta}\nAssistente: ";
+                var requestBody = new
+                {
+                    prompt = prompt,
+                    max_tokens = 50
+                };
+
+                var jsonRequest = JsonConvert.SerializeObject(requestBody);
+                var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                var response = httpClient.PostAsync("https://api.openai.com/v1/chat/completions", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = response.Content.ReadAsStringAsync().Result;
+                    var result = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
+                    resposta = result.choices[0].text;
+                }
+            }
+
+            return resposta;
+        }
+        /*
+         * 
+        private string PesquisarComCatGPT(string pergunta)
         {
             string APIKey = "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";
             string answer = string.Empty;
@@ -134,7 +182,7 @@ namespace JsonVai.Controllers
 
             return answer;
         }
-
+        */
     }
 
 }
